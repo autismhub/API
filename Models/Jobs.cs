427421920c59376024fda4d185e49334a7c38ecb
Mikkel Hartig Braunstein <mikk151g@edu.campusvejle.dk>
2020-09-15 09:37:08 +0200

    Add Job model

---
+++ Models/Jobs.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Jobs
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _title;
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        private DateTime _creationDate;
+
+        public DateTime CreationDate
+        {
+            get { return _creationDate; }
+            set { _creationDate = value; }
+        }
+
+        private string _logo;
+
+        public string Logo
+        {
+            get { return _logo; }
+            set { _logo = value; }
+        }
+
+        private string _companyName;
+
+        public string CompanyName
+        {
+            get { return _companyName; }
+            set { _companyName = value; }
+        }
+
+        private string _category;
+
+        public string Category
+        {
+            get { return _category; }
+            set { _category = value; }
+        }
+
+        private string _city;
+
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+
+        private string _description;
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        private string _benefits;
+
+        public string Benefits
+        {
+            get { return _benefits; }
+            set { _benefits = value; }
+        }
+
+        private string _companyImage;
+
+        public string CompanyImage
+        {
+            get { return _companyImage; }
+            set { _companyImage = value; }
+        }
+
+        private int _email;
+
+        public int Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        private string _phone;
+
+        public string Phone
+        {
+            get { return _phone; }
+            set { _phone = value; }
+        }
+
+        private string _address;
+
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+
+        private string _contactName;
+
+        public string ContactName
+        {
+            get { return _contactName; }
+            set { _contactName = value; }
+        }
+
+    }
+}
