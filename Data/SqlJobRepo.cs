baa0efef6dc0200210eb3f2d0d807b316e7a2459
Mikkel Hartig Braunstein <mikk151g@edu.campusvejle.dk>
2020-09-16 14:54:13 +0200

    Replace MockJobRepo with a real repository

---
+++ Data/SqlJobRepo.cs
@@ -0,0 +1,28 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class SqlJobRepo : IJobRepo
+    {
+        private readonly JobContext _context;
+
+        public SqlJobRepo(JobContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Job> GetAllJobs()
+        {
+            return _context.Jobs.ToList();
+        }
+
+        public Job GetJobById(int id)
+        {
+            return _context.Jobs.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
