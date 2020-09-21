9247c3300f9ba929f635a119478571e7f2d64b4d
mads (matt2256) <mads@mmm-m.dk>
2020-09-21 13:24:43 +0200

    Added some code to SqlUserRepo

--- Data - User/SqlUserRepo.cs
+++ Data - User/SqlUserRepo.cs
@@ -1,4 +1,5 @@
-﻿using API.Models;
+﻿using API.Data;
+using API.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,24 +9,36 @@ namespace API.Data___User
 {
     public class SqlUserRepo : IUserRepo
     {
+        private readonly JobContext _context;
+
+        public SqlUserRepo(JobContext context)
+        {
+            _context = context;
+        }
+
         public void CreateUser(User user)
         {
-            throw new NotImplementedException();
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            _context.Users.Add(user);
         }
 
         public IEnumerable<User> GetAllUsers()
         {
-            throw new NotImplementedException();
+            return _context.Users.ToList();
         }
 
         public User GetUserById(int id)
         {
-            throw new NotImplementedException();
+            return _context.Users.FirstOrDefault(p => p.Id == id);
         }
 
         public bool SaveChanges()
         {
-            throw new NotImplementedException();
+            return (_context.SaveChanges() >= 0);
         }
     }
 }
