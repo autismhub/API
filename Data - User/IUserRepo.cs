b3f739ec8065eef4149122ab9998097fc1de557e
mads (matt2256) <mads@mmm-m.dk>
2020-09-21 11:07:32 +0200

    Made a interface for user handeling

---
+++ Data - User/IUserRepo.cs
@@ -0,0 +1,15 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data___User
+{
+    interface IUserRepo
+    {
+        IEnumerable<User> GetAllUsers();
+
+        User GetUserById(int id);
+    }
+}
